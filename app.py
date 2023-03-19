81a862044fddd6b0dad7bd9fe3b530d9714136e7
Atul <code.atul01@gmail.com>
2023-03-19 16:42:47 +0530

    using app.py for the firebase part

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
 from flask import *
+from pyrebase import *
 
+#---------------initializeing application------------------------
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
+#---------------configuring firebase----------------------------
+
+firebaseConfig = {'apiKey': "AIzaSyAnxsGxp6e5nqH8L4y5z926N_xD3UEq0W0",
+  'authDomain': "eduhive-b888d.firebaseapp.com",
+  'projectId': "eduhive-b888d",
+  'storageBucket': "eduhive-b888d.appspot.com",
+  'messagingSenderId': "949663329841",
+  'appId': "1:949663329841:web:6840f25200d1954474ef10",
+  'measurementId': "G-ELN6NQQ4TX",
+  'databaseURL': ""}
+
+firebase = initialize_app(firebaseConfig)
+
+auth = firebase.auth()
 
 @app.route('/')
 def home():
